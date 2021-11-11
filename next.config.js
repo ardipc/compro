10ac94de312116dd0b37212df24a9131d7bc0a36
Ahmad Ardiansyah <ardiansyah3ber@gmail.com>
2021-11-11 14:31:56 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,6 @@ module.exports = {
     domains: ['eskipaper.com', 'miro.medium.com', 'doitbeforeme.com', 'images8.alphacoders.com']
   },
   env: {
-    mongodburl: 'mongodb+srv://mongodb-stitch-remotecars-drvsi:terLalu7@remotecars1.0nn52.mongodb.net/compro?retryWrites=true&w=majority'
+    mongodburl: ''
   }
 }
